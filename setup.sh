b629d3158785f5793ed23400ae562d60546a0e0f
Pelle Johnsen <p.johnsen@globalscanning.com>
2019-03-01 12:39:27 +0100

    Add crontab

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ ufw allow 27020/udp
 cd /home/csgoserver
 chown csgoserver:csgoserver serverfiles/csgo/cfg/*.cfg
 su - csgoserver -c './csgoserver restart'
+
+# cron
+crontab -u csgoserver - <<"EOF"
+#Server Name
+*/5 * * * * /home/username/csgoserver monitor > /dev/null 2>&1
+*/30 * * * * /home/username/csgoserver update > /dev/null 2>&1
+00 6 * * *  /home/username/csgoserver force-update > /dev/null 2>&1
+0 0 * * 0 /home/username/csgoserver update-functions > /dev/null 2>&1
+EOF
